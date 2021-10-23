e9b2f4102f3bbc550d16d22bbf92ea1ae2f4fcea
cheichey <cheichey.1223@gmail.com>
2021-10-24 00:16:05 +0900

    :wrench:web: webpack.config.js を追加した

---
+++ web/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+
+  entry: "./src/index.tsx",
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    publicPath: "/"
+  },
+
+  resolve: {
+    extensions: [".js", ".json", ".ts", ".tsx"],
+  },
+
+  devtool: "source-map",
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: `${__dirname}/static`
+    },
+    hot: true,
+    historyApiFallback: true,
+  },
+
+  plugins: [
+  ]
+};
