0e57b5d79e0f129187e1ac86c2dbf16e38007f46
cheichey <cheichey.1223@gmail.com>
2021-11-07 13:00:23 +0900

    :wrench:server: Swagger, Swagger-UI を導入した

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = new ConfigService();
   const port = configService.get('API_PORT');
+
+  const documentConfig = new DocumentBuilder()
+    .setTitle('Kakabot')
+    .setDescription('The Kakabot API description')
+    .setVersion('0.0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, documentConfig);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(port);
 }
 bootstrap();
--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -9,22 +9,41 @@ import {
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create.user.dto';
+import {
+  ApiCreatedResponse,
+  ApiFoundResponse,
+  ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { plainToClass } from 'class-transformer';
+import { UserDto } from './dto/user.dto';
 
+@ApiTags('user')
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Get()
+  @ApiResponse({ status: 200, description: '成功しました' })
   getUsers() {
-    return this.userService.getUsers();
+    const users = this.userService.getUsers();
+    return plainToClass(UserDto, users);
   }
 
   @Get('/:id')
+  @ApiFoundResponse({ description: '成功しました。' })
+  @ApiNotFoundResponse({ description: '存在しないユーザーです。' })
   getUserById(@Param('id') id: number) {
     return this.userService.getUserById(id);
   }
 
   @Post()
+  @ApiCreatedResponse({ description: 'ユーザーの作成に成功しました。' })
+  @ApiInternalServerErrorResponse({
+    description: 'サーバー内部でのエラーです。',
+  })
   @UsePipes(ValidationPipe)
   createUser(@Body() createUserDto: CreateUserDto) {
     return this.userService.createUser(createUserDto);
