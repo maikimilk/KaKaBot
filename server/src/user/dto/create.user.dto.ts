1b3b0f3067e5af0755cce40f4d074b8ce2f86e94
cheichey <cheichey.1223@gmail.com>
2021-11-07 13:00:23 +0900

    :wrench:server: Swagger, Swagger-UI を導入した

--- server/src/user/dto/create.user.dto.ts
+++ server/src/user/dto/create.user.dto.ts
@@ -1,10 +1,13 @@
 import { UserDto } from './user.dto';
 import { IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto extends UserDto {
+  @ApiProperty()
   @IsNotEmpty()
   password: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   displayName: string;
 }
